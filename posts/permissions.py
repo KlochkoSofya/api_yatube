e115ccfe8d03e628ef52a2cdf13abc4c2d831d8e
Sofya <sofya.s.frolova@gmail.com>
2021-04-05 12:21:01 +0300

    AfterPEP8check

--- posts/permissions.py
+++ posts/permissions.py
@@ -8,5 +8,5 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     """
     def has_object_permission(self, request, view, obj):
         if (request.method in permissions.SAFE_METHODS 
-            or obj.author == request.user):
+                or obj.author == request.user):
             return True
